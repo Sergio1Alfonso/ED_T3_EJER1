afec2b869d0134fbade4626633e547e0a7986eb2
Sergio <sergio1.alfonso.gonzalo@gmail.com>
2022-01-26 09:03:28 +0100

    He añadido javadoc al fichero ED_T3_EJER1\Sergio José Alfonso Gonzalo\Tema 05\src\main\java\ejemplos\Ejemplo 1.java

--- Sergio José Alfonso Gonzalo/Tema 05/src/main/java/ejemplos/Ejemplo1.java
+++ Sergio José Alfonso Gonzalo/Tema 05/src/main/java/ejemplos/Ejemplo1.java
@@ -3,10 +3,15 @@ package ejemplos;
 import java.util.Map;
 import java.util.TreeMap;
 
+/**
+ * Primer ejemplo del uso de map
+ */
 public class Ejemplo1 {
     public static void main(String[] args) {
+        // Crear un TreeMap con clave y dato de tipo Integer
         Map<Integer, Integer> ocurrencias = new TreeMap<>();
 
+        // Añadir 1000 números aleatorios con la función doHickey y añadirlo al mapa
         for (int i = 0; i < 1000; i++) {
             int n = doHickey(i);
 
@@ -14,9 +19,17 @@ public class Ejemplo1 {
             ocurrencias.put(n, ocurrencias.get(n) + 1);
         }
 
+        // Contar el número de veces que aparece cada número y mostrarlo por consola
         for (int n : ocurrencias.keySet()) System.out.println(n + ": " + ocurrencias.get(n));
     }
 
+    /**
+     * Generar un número pseudo-aleatorio positivo con una distribución
+     * cercana a 0 dado otro número preferiblemente de forma secuencial
+     *
+     * @param n parametro que recibe como número (da igual el número que sea pero se recomienda que sea secuencial)
+     * @return un número pseudo-aleatorio positivo con una distribución cercana a 0
+     */
     public static int doHickey(int n) {
         n += Math.random() * n;
         n -= Math.random() * n;
